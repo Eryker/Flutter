d8ec52ac212bc004a6df9ff66afd8cbacd272b87
Oumar <oumar@MacBook-Pro-de-Oumar.local>
2022-06-08 15:18:51 +0200

    Import player page to main

--- lib/player_page.dart
+++ lib/player_page.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class PlayerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
